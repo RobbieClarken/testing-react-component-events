c4088def212ef932116abb3ea88e43a2d8d4c51b
RobbieClarken <robbie.clarken@gmail.com>
2017-11-19 10:44:12 +1100

    Try testing <AutoSavingInput /> Enter key press with Enzyme

--- src/AutoSavingInput.test.js
+++ src/AutoSavingInput.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactTestUtils from 'react-dom/test-utils'
+import { shallow, mount } from 'enzyme'
 
 import AutoSavingInput from './AutoSavingInput'
 
@@ -14,28 +15,69 @@ function setup () {
   return { component, node, onSubmit }
 }
 
-it('submits when the input is defocussed', () => {
-  const { node, onSubmit } = setup()
-  ReactTestUtils.Simulate.blur(node)
-  expect(onSubmit).toHaveBeenCalledWith('the-value')
+describe('<AutoSavingInput />', () => {
+
+  it('submits when the input is defocussed', () => {
+    const { node, onSubmit } = setup()
+    ReactTestUtils.Simulate.blur(node)
+    expect(onSubmit).toHaveBeenCalledWith('the-value')
+  })
+
+  // THIS TEST SHOULD PASS BUT IT FAILS
+  it('submits when Enter key is pressed', () => {
+    const { node, onSubmit } = setup()
+    ReactTestUtils.Simulate.keyUp(node, {key: 'Enter'})
+    expect(onSubmit).toHaveBeenCalledWith('the-value')
+  })
+
+  // THIS TEST SHOULD FAIL BUT IT PASSES
+  it('does not submit when Escape key is pressed', () => {
+    const { node, onSubmit } = setup()
+    ReactTestUtils.Simulate.keyUp(node, {key: 'Escape'})
+    expect(onSubmit).toHaveBeenCalledTimes(0)
+  })
+
+  it('the currentValue to be reset on Escape key', () => {
+    const { component, node, onSubmit } = setup()
+    ReactTestUtils.Simulate.keyUp(node, {key: 'Escape'})
+    expect(component.state.currentValue).toEqual('last-saved-value')
+  })
+
 })
 
-// THIS TEST SHOULD PASS BUT IT FAILS
-it('submits when Enter key is pressed', () => {
-  const { node, onSubmit } = setup()
-  ReactTestUtils.Simulate.keyUp(node, {key: 'Enter'})
-  expect(onSubmit).toHaveBeenCalledWith('the-value')
-})
 
-// THIS TEST SHOULD FAIL BUT IT PASSES
-it('does not submit when Escape key is pressed', () => {
-  const { node, onSubmit } = setup()
-  ReactTestUtils.Simulate.keyUp(node, {key: 'Escape'})
-  expect(onSubmit).toHaveBeenCalledTimes(0)
-})
+/****************** ENZYME ***********************/
 
-it('the currentValue to be reset on Escape key', () => {
-  const { component, node, onSubmit } = setup()
-  ReactTestUtils.Simulate.keyUp(node, {key: 'Escape'})
-  expect(component.state.currentValue).toEqual('last-saved-value')
+describe('when testing with enzyme', () => {
+
+  describe('using shallow', () => {
+
+    // THIS TEST SHOULD PASS BUT IT FAILS
+    it('<AutoSavingInput /> submits when Enter key is pressed', () => {
+      const onSubmit = jest.fn()
+      const component = shallow(
+        <AutoSavingInput lastSavedValue="last-saved-value" onSubmit={onSubmit} />
+      )
+      component.setState({currentValue: 'the-value'})
+      component.find('input').simulate('keyUp', {key: 'Enter'})
+      expect(onSubmit).toHaveBeenCalledWith('the-value')
+    })
+
+  })
+
+  describe('using mount', () => {
+
+    // THIS TEST SHOULD PASS BUT IT FAILS
+    it('<AutoSavingInput /> submits when Enter key is pressed', () => {
+      const onSubmit = jest.fn()
+      const component = mount(
+        <AutoSavingInput lastSavedValue="last-saved-value" onSubmit={onSubmit} />
+      )
+      component.setState({currentValue: 'the-value'})
+      component.find('input').simulate('keyUp', {key: 'Enter'})
+      expect(onSubmit).toHaveBeenCalledWith('the-value')
+    })
+
+  })
+
 })
